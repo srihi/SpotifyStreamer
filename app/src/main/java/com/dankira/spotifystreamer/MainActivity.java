7e1287d5bb9d358a9fe189578abea5553b1049df
Dawit Alemu <dawit_alemu@sensimed.ch>
2015-06-18 17:41:12 +0200

    Work in progress - Orientation change handling.

--- app/src/main/java/com/dankira/spotifystreamer/MainActivity.java
+++ app/src/main/java/com/dankira/spotifystreamer/MainActivity.java
@@ -1,16 +1,24 @@
 package com.dankira.spotifystreamer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment mContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            //Restore the fragment's instance
+            mContent = getFragmentManager().getFragment(savedInstanceState, "mContent");
+        }else{
+            mContent = getFragmentManager().findFragmentById(R.id.fragment);
+        }
     }
 
 
@@ -18,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
         return true;
     }
 
@@ -34,5 +44,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+//Save the fragment's instance
+        getFragmentManager().putFragment(outState, "mContent", mContent);
+
+
     }
 }
--- app/src/main/java/com/dankira/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/dankira/spotifystreamer/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package com.dankira.spotifystreamer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -24,6 +27,7 @@ import kaaes.spotify.webapi.android.SpotifyService;
 import kaaes.spotify.webapi.android.models.AlbumsPager;
 import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.RetrofitError;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -33,11 +37,29 @@ public class MainActivityFragment extends Fragment {
     SpotifyArtistListAdapter searchResultListAdapter;
     ArrayList<ArtistInfo> searchResultArray;
     String searchTerm = "Jay Z";
+    Toast toast;
 
     public MainActivityFragment() {
     }
 
-//    @Override
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        outState.putSerializable("SEARCHRESULTARRAY",searchResultArray);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if(savedInstanceState != null)
+        {
+            searchResultArray = (ArrayList<ArtistInfo>)savedInstanceState.getSerializable("SEARCHRESULTARRAY");
+            searchResultListAdapter.notifyDataSetChanged();
+        }
+    }
+
+    //    @Override
 //    public void onStart() {
 //        super.onStart();
 //        searchResultArray.clear();
@@ -101,7 +123,23 @@ public class MainActivityFragment extends Fragment {
         }
         return fragmentView;
     }
+    private void ShowCustomToast(String textMessage) {
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View toastLayout = inflater.inflate(R.layout.custom_toast, (ViewGroup) getView().findViewById(R.id.toast_layout_root));
 
+        TextView toastTextView = (TextView) toastLayout.findViewById(R.id.toast_textArea);
+        toastTextView.setText(textMessage);
+
+        if (toast != null) {
+            toast.cancel();
+        }
+        toast = new Toast(getActivity().getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(toastLayout);
+        toast.show();
+
+    }
     public class SpotifySearchTask extends AsyncTask<String,Void,Void> {
 
         @Override
@@ -109,30 +147,48 @@ public class MainActivityFragment extends Fragment {
             SpotifyApi api = new SpotifyApi();
 
             SpotifyService spotify = api.getService();
+            try {
+                ArtistsPager artistsPager = spotify.searchArtists(params[0]);
+                int i = 0;
 
-            ArtistsPager artistsPager = spotify.searchArtists(params[0]);
-            int i = 0;
+                if (artistsPager.artists.items.size() == 0) {
 
-            for (Artist s : artistsPager.artists.items) {
-                final ArtistInfo info = new ArtistInfo();
-                info.setArtistName(s.name);
-                info.setArtistId(s.id);
-                if (s.images.size() != 0) {
-                    info.setArtistImageUrl(s.images.get(0).url);
+                    getActivity().runOnUiThread((new Runnable() {
+                        @Override
+                        public void run() {
+                            ShowCustomToast("There are no artists maching search criteria.");
+                        }
+                    }));
+
+                    return null;
                 }
-                else {
-                    AlbumsPager albumsPager = spotify.searchAlbums(s.name);
-                    if (albumsPager.albums.items.size() > 0) {
-                        info.setArtistImageUrl(albumsPager.albums.items.get(0).images.get(0).url);
+
+                for (Artist s : artistsPager.artists.items) {
+                    final ArtistInfo info = new ArtistInfo();
+                    info.setArtistName(s.name);
+                    info.setArtistId(s.id);
+                    if (s.images.size() != 0) {
+                        info.setArtistImageUrl(s.images.get(0).url);
+                    } else {
+                        AlbumsPager albumsPager = spotify.searchAlbums(s.name);
+                        if (albumsPager.albums.items.size() > 0) {
+                            info.setArtistImageUrl(albumsPager.albums.items.get(0).images.get(0).url);
+                        } else {
+                            Log.v("SpotifyStreamer", "No images found for ....." + s.name);
+                        }
                     }
-                    else
-                    {
-                        Log.v("SpotifyStreamer","No images found for ....."+s.name);
-                    }
+                    info.setArtistName(s.name);
+                    searchResultArray.add(i, info);
+                    i++;
                 }
-                info.setArtistName(s.name);
-                searchResultArray.add(i, info);
-                i++;
+            } catch (final RetrofitError error) {
+                getActivity().runOnUiThread((new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }));
+
             }
             return null;
         }
